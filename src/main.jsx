85ea0f37315179635170341da734cd7d8ec0a2b9
Chakrit <chakritkp@hotmail.com>
2023-10-12 15:05:58 +0700

    delete router

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom';
 
-import App from './App.jsx'
 import './index.css'
-import Home from './Page/Home.jsx';
-import Contact from './Component/Contact.jsx';
-import Skill from './component/Skill.jsx';
-import Project from './component/Project.jsx';
 
-const router = createBrowserRouter([
-  {path:'/', element: <Home/>},
-  {path:'/contact', element: <Contact/>},
-  {path:'/skill', element: <Skill/> },
-  {path:'/project', element: <Project/> }
-])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
