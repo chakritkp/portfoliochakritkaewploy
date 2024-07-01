b249001cfdd2da7fde0218b505c27e43af151f81
Chakrit <chakritkp@hotmail.com>
2024-07-01 15:07:56 +0700

    update summery

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -19,10 +19,7 @@ const Hero = () => {
           <article className="m-auto min-w-[300px] max-w-[500px] flex flex-col lg:items-start">
             <h2 className="text-center text-3xl font-bold mb-3">SUMMARY</h2>
             <p className="px-5 text-base font-medium text-justify overflow-auto h-[180px] lg:px-0 lg:overflow-hidden lg:h-full">
-            Hello, I'm Chakrit Kaewploy, a Software Developer. Previously, I worked as a civil servant at NEA under the Ministry of Commerce, overseeing procurement projects for the NEA and E-Academy websites for four years. Following that, I was selected to join the 5th edition of the Junior Software Developer program, a boot camp designed for career changers aspiring to become Software Developers. The program focused on teaching the MERN Stack, and participants were required to complete a project before graduating.
-            <br/>
-            In my project, I was responsible for the User system, developing features for Login, Register, and Update functionalities on both the Frontend and Backend. Currently, I am actively seeking opportunities in Frontend Development and Fullstack Development. In the future, I plan to further enhance my skills and pursue a career as a Mobile Developer.
-            </p>
+            Hello, my name is Chakrit Kaewploy. I am a career switcher who graduated from Generation Thailand - Junior Software Developer program. I have work experience as a Software Developer at Maholan Company. My technical skills include React.js, useForm, MUI, Nest.js, TypeORM, and SQL. My responsibilities involved gathering requirements from Business Analysts (BAs) to create data entry forms and display information according to specified formats and functions. I developed APIs to retrieve and create necessary data. Currently, I am studying Docker and microservices to further enhance my skills.            </p>
           </article>
         </div>
 
