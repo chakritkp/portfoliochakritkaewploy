8474c6ddc2340bb57bedb758c7c028ce96315119
Chakrit <chakritkp@hotmail.com>
2024-07-16 16:46:03 +0700

    change CV

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
         <UilLinkedin size="3rem" color="#61DAFB" />
       </a>
       <a
-        href="https://drive.google.com/file/d/1UH4FpsJlmqqKgPIw53FF_V-iIHBMV8QC/view?usp=sharing"
+        href="https://drive.google.com/file/d/1Wj7rOt7H9Uj_hgp9ckW1bpaq0RO9vsC9/view?usp=sharing"
         target="_blank"
       >
         <UilFileExport size="3rem" color="#61DAFB" />
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -9,7 +9,6 @@ const Hero = () => {
       className="h-screen flex justify-center items-center pt-[70px]"
     >
       <div className="lg:flex justify-center items-center gap-5">
-
         <div className="mx-auto w-60 lg:w-72 mb-5">
           <img className="w-fit rounded-lg" src={Profileimg} alt="Profile" />
           <Contact />
@@ -19,10 +18,17 @@ const Hero = () => {
           <article className="m-auto min-w-[300px] max-w-[500px] flex flex-col lg:items-start">
             <h2 className="text-center text-3xl font-bold mb-3">SUMMARY</h2>
             <p className="px-5 text-base font-medium text-justify overflow-auto h-[180px] lg:px-0 lg:overflow-hidden lg:h-full">
-            Hello, my name is Chakrit Kaewploy. I am a career switcher who graduated from Generation Thailand - Junior Software Developer program. I have work experience as a Software Developer at Maholan Company. My technical skills include React.js, useForm, MUI, Nest.js, TypeORM, and SQL. My responsibilities involved gathering requirements from Business Analysts (BAs) to create data entry forms and display information according to specified formats and functions. I developed APIs to retrieve and create necessary data. Currently, I am studying Docker and microservices to further enhance my skills.            </p>
+              I am a career switcher who participated in Generation Thailand -
+              Junior Software Developer program. I have recently worked as a
+              Software Developer at Maholan Company. My technical skills include
+              React.js, useForm, MUI, Nest.js, TypeORM, and SQL. My
+              responsibilities involved gathering requirements from Business
+              Analysts (BAs) to create data entry forms and display information
+              according to specified formats and functions. I developed APIs to
+              retrieve and create necessary data
+            </p>
           </article>
         </div>
-
       </div>
     </section>
   );
